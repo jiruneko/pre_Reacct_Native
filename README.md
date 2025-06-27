1e656d8c66fe8f9176ab452cc3b34041f7d33e59
jiruneko <oohikaru310@gmail.com>
2025-06-27 11:09:02 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# pre_Reacct_Native
---
+++ main.js
@@ -0,0 +1,38 @@
+const a = 2
+console.log(a);
+
+let b = 3
+console.log(b);
+
+b = 4
+console.log(b);
+
+const greeting = "Hello"
+console.log(greeting);
+console.log(typeof(greeting))
+
+const obj = {a: 1, b: 2}
+console.log(obj.a)
+console.log(obj.b)
+
+const array = ["a", "b", "c"]
+console.log(array)
+console.log(array[0])
+
+let d
+console.log(d);
+
+function square(a) {
+    return a * a
+}
+console.log(square(8))
+
+function sayHello() {
+    console.log("Hello World");
+}
+sayHello()
+
+const squarez = (c) => {
+    return c * c
+}
+console.log(squarez(9));
